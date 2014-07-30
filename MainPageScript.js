0ee12f92cb03cfa37fd397daefda2e55ef3df58d
chivagarg <chivagarg@gmail.com>
2014-07-30 00:24:39 -0700

    Hooked up list
    Suggestion items added programmatically

--- MainPageScript.js
+++ MainPageScript.js
@@ -1,12 +1,24 @@
 function QueryItemFromNutritionX()
 {
-  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "query": "Cheddar Cheese"}';
+  var query = document.getElementById('id_itemName').value;
+  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "limit":"10", "fields":["item_name", "nf_serving_size_unit"], "query":"' + query + '"}';
   var url = "https://api.nutritionix.com/v1_1/search";
   CreateAndSendRequest("POST",paramString,url,function()
   {
     if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-      document.getElementById("id_suggestionspanel").innerHTML=xmlhttp.responseText;
+      var list = document.getElementById('id_olist');
+      var hitsArray = JSON.parse(xmlhttp.responseText).hits;
+
+      for (var i = 0; i < hitsArray.length; i++)
+      {
+        var listItem = document.createElement('li');
+        listItem.appendChild(document.createTextNode(hitsArray[i].fields.item_name));
+        list.appendChild(listItem);
+      }
+
+
+      $("#id_LoadResponseTxt").hide();
     }
   });
 }
