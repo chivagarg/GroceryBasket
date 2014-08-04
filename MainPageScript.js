e7e2f96564f171b010730e714d1021ef2d8e9727
chivagarg <chivagarg@gmail.com>
2014-08-03 18:20:02 -0700

    More functionality

--- MainPageScript.js
+++ MainPageScript.js
@@ -1,26 +1,47 @@
+var isResolved = false;
+var currentServingSizeUnit;
 function QueryItemFromNutritionX()
 {
   var query = document.getElementById('id_itemName').value;
-  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "limit":"10", "fields":["item_name", "nf_serving_size_unit"], "query":"' + query + '"}';
+  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "limit":"10", "fields":["item_name", "brand_name", "nf_serving_size_unit", "nf_serving_size_qty", "nf_servings_per_container"], "query":"' + query + '"}';
   var url = "https://api.nutritionix.com/v1_1/search";
   CreateAndSendRequest("POST",paramString,url,function()
   {
     if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-      var list = document.getElementById('id_olist');
+      var list = document.getElementById('id_ulist');
       var hitsArray = JSON.parse(xmlhttp.responseText).hits;
+
 
       for (var i = 0; i < hitsArray.length; i++)
       {
         var listItem = document.createElement('li');
-        listItem.appendChild(document.createTextNode(hitsArray[i].fields.item_name));
+        var textToDisplay = hitsArray[i].fields.item_name + " | " + hitsArray[i].fields.brand_name + " | " + " | " + hitsArray[i].fields.nf_serving_size_unit + " | " +  hitsArray[i].fields.nf_serving_size_unit + " | " + hitsArray[i].fields.nf_servings_per_container;
+        listItem.appendChild(document.createTextNode(textToDisplay));
         list.appendChild(listItem);
       }
 
-
       $("#id_LoadResponseTxt").hide();
+      AddClickEventToListItems();
     }
   });
+}
+
+function AddClickEventToListItems()
+{
+  var lis = document.getElementById("id_ulist").getElementsByTagName('li');
+
+  for (var i=0; i<lis.length; i++) {
+    lis[i].addEventListener('click', SetSelectedText, false);
+  }
+
+  function SetSelectedText() {
+    if (!isResolved)
+    {
+      isResolved = true;
+      document.getElementById('id_itemName').value = this.innerHTML;
+    }
+  }
 }
 
 function CreateRequestObject()
@@ -53,7 +74,7 @@ function CreateAndSendRequest(requestType,paramString,url,cfunc)
 function ItemNameResolver()
 {
   var query = document.getElementById('id_itemName').value;
-  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "limit":"10", "fields":["item_name", "nf_serving_size_unit"], "query":"' + query + '"}';
+  var paramString='{"appId":"9ca93004", "appKey": "769a7bfd0563bf4e30441f56c4c833e2", "limit":"10", "fields":["item_name", "brand_name", "nf_serving_size_unit"], "query":"' + query + '"}';
   var url = "https://api.nutritionix.com/v1_1/search";
   CreateAndSendRequest("POST",paramString,url,function()
   {
